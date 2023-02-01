0a4745170c3ccc7b554633caf0bd49829a132cc2
Mariyan Maksimov <mariyan.pickup@gmail.com>
2023-02-01 20:36:11 +0200

    removed comments

--- ag-grid/src/app/app.component.ts
+++ ag-grid/src/app/app.component.ts
@@ -17,34 +17,28 @@ export class AppComponent {
     { field: 'price' },
   ];
 
-  // DefaultColDef sets props common to all Columns
   public defaultColDef: ColDef = {
     sortable: true,
     filter: true,
     resizable: true,
   };
 
-  // Data that gets displayed in the grid
   public rowData$!: Observable<any[]>;
 
-  // For accessing the Grid's API
   @ViewChild(AgGridAngular) agGrid!: AgGridAngular;
 
   constructor(private http: HttpClient) {}
 
-  // Example load data from sever
   onGridReady(params: GridReadyEvent) {
     this.rowData$ = this.http.get<any[]>(
       'https://www.ag-grid.com/example-assets/row-data.json'
     );
   }
 
-  // Example of consuming Grid Event
   onCellClicked(e: CellClickedEvent): void {
     console.log('cellClicked', e);
   }
 
-  // Example using Grid's API
   clearSelection(): void {
     this.agGrid.api.deselectAll();
   }
